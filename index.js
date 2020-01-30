bc15f1c46719e0ee7dcb21548aa8addc2ce3108b
Tjalling Tolle <tjallingt@gmail.com>
2020-01-30 15:31:37 +0100

    update-ruleset

--- index.js
+++ index.js
@@ -12,24 +12,62 @@ module.exports = {
   ],
 
   "rules": {
-    "node/no-missing-require": ["error", {"allowModules": ["homey"]}],
-    "node/exports-style": ["error", "module.exports"],
+    "node/no-missing-require": ["error", { "allowModules": ["homey"] }],
+
     "no-underscore-dangle": "off",
-    "max-len": ["warn", { "code": 300 }],
-    "consistent-return": "off",
+
     "no-bitwise": "off",
-    "no-param-reassign": ["warn"],
-    "no-await-in-loop": "off",
-    "no-empty": "off",
+
+    "no-param-reassign": "off",
+
+    "no-empty": ["error", { "allowEmptyCatch": true }],
+
     "no-plusplus": "off",
-    "no-shadow": "off",
-    "no-process-exit": "off",
-    "arrow-parens": "off",
-    "arrow-body-style": "off",
-    "curly": "off",
-    "prefer-destructuring": "off",
+
+    "arrow-parens": ["error", "as-needed"],
+
     "class-methods-use-this": "off",
-    "no-continue": "off"
+
+    "no-continue": "off",
+
+    "dot-notation": "off",
+
+    'space-before-function-paren': [
+      "error",
+      { "anonymous": "never", "named": "never", "asyncArrow": "always" }
+    ],
+
+    "no-unused-vars": [
+      "error",
+      { "vars": "all", "args": "none", "ignoreRestSiblings": true }
+    ],
+
+    "padded-blocks": [
+      "error",
+      { "blocks": "never", "switches": "never", "classes": "always" }
+    ],
+
+    "nonblock-statement-body-position": [
+      "error",
+      "beside",
+      { "overrides": { "if": "any" } }
+    ],
+
+    "no-restricted-syntax": [
+      "error",
+      {
+        "selector": "ForInStatement",
+        "message": "for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array."
+      },
+      {
+        "selector": "LabeledStatement",
+        "message": "Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand."
+      },
+      {
+        "selector": "WithStatement",
+        "message": "`with` is disallowed in strict mode because it makes code impossible to predict and optimize."
+      }
+    ],
   },
 
   "settings": {
